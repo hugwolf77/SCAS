72f37b8ee6ecdc955b51924c3eb9efabe0332d60
Augustine77 <hugwolf77@gmail.com>
2024-08-11 16:13:04 +0900

    matplotlib font hangual

--- corpRch_ecoAct/FactorAnly.py
+++ corpRch_ecoAct/FactorAnly.py
@@ -3,6 +3,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+plt.rc('font', family='NanumBarunGothic') 
+plt.rcParams['axes.unicode_minus'] =False
+
+# scaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 # normal distribution test
 from scipy.stats import shapiro, kstest
@@ -16,14 +22,26 @@ from factor_analyzer.factor_analyzer import calculate_kmo, calculate_bartlett_sp
 from factor_analyzer.factor_analyzer import FactorAnalyzer
 from statsmodels.multivariate.factor import Factor
 
-df = pd.read_excel('./data/분석_데이터_목록.xlsx', sheet_name='variable', index_col='date')
 
-df.plot()
+# data load
+df = pd.read_excel('./data/분석_데이터_목록.xlsx', sheet_name='transData', index_col='date')
+
+# scaling
+# scaler = StandardScaler()
+# scaled = scaler.fit_transform(df)
+mean = df.mean()
+std = df.std()
+scaled = (df - mean)/std 
+
+# plot
+df_std = scaled.melt(var_name='Column', value_name='Normalized')
+plt.figure(figsize=(12, 6))
+ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+_ = ax.set_xticklabels(df.keys(), rotation=90)
 plt.show()
-print(df.head())
 
-df = df.values
-chi_square_value,p_value=calculate_bartlett_sphericity(df)
+# norm dist test
+chi_square_value,p_value=calculate_bartlett_sphericity(scaled)
 print(f"chi_square_value : {chi_square_value}")
 print(f"p_value() :{p_value}")
 
